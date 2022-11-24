baa4258ad0ecd8a2ccb2ca25d430cf75a56235b4
rgandhi715 <65450546+rgandhi715@users.noreply.github.com>
2022-11-24 09:30:09 +0000

    corrected location

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     bucket         = "aws-one-click"
     #key            = "$($env:env)/($env:tfstat_loc)"
     key            = "codepipeline/dev/foundation/vpc.tfstate"
-    region         = "ap=south-1"
+    region         = "ap-south-1"
     #Replace this with your DynamoDB table name!
     #dynamodb_table = "$($env:dynamo_db)"
     encrypt        = true
